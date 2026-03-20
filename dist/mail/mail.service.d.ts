a4b5da2e98a047b0c387c88613d96040d07aec7e
babuvignesh1106-hash <babuvignesh1106@gmail.com>
2026-03-20 14:02:49 +0530

    add the new one for neon

--- dist/mail/mail.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class MailService {
-    private configService;
-    constructor(configService: ConfigService);
-    private transporter;
-    sendResetPasswordEmail(to: string, token: string): Promise<void>;
-}
